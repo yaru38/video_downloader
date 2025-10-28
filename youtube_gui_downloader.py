af598a86a2366decb4b8bf0bf2b6ba363bbc3e40
yaru38 <yaru61087@gmail.com>
2025-10-29 01:34:22 +0900

    mp4がDLできなかった不具合を修正

--- youtube_gui_downloader.py
+++ youtube_gui_downloader.py
@@ -85,10 +85,35 @@ def download_video(urls, mode):
             'no_warnings': True,
         }
     elif mode == 'mp4':
+        try:
+            target_height = int(config.get("video_quality", default_config["video_quality"]))
+        except (TypeError, ValueError):
+            target_height = int(default_config["video_quality"])
+
+        # 映像・音声ともに mp4/m4a が無いケースが多いので柔軟にフォールバックする
+        video_candidates = [
+            f"bv*[ext=mp4][height<={target_height}]",
+            f"bv*[height<={target_height}]",
+            "bv*[ext=mp4]",
+            "bv*",
+        ]
+        audio_candidates = [
+            "ba[ext=m4a]",
+            "ba",
+        ]
+
+        format_chain = [f"{v}+{a}" for v in video_candidates for a in audio_candidates]
+        format_chain.extend(["b[ext=mp4]", "best"])
+        format_selector = "/".join(format_chain)
+
         options = {
-            'format': f'bestvideo[ext=mp4][height>={config["video_quality"]}]+bestaudio[ext=m4a]/best[ext=mp4][height>={config["video_quality"]}]',
+            'format': format_selector,
             'outtmpl': output_template,
             'merge_output_format': 'mp4',
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4',  # yt-dlp側の綴りに合わせる
+            }],
             'progress_hooks': [progress_hook],
             'no_warnings': True,
         }
